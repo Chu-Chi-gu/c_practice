ae689157f09b7e44ddb47e63debacab6216cbe14
Lacie <asdf4865123@gmail.com>
2025-06-17 23:43:35 +0800

    "leetcode380更新"

--- leetcode380.c
+++ leetcode380.c
@@ -1,14 +1,62 @@
 #include <stdlib.h>
-struct s1 {
-    int a;
-    char b;
-    short c;
+#include <stdbool.h>
+#include "uthash.h"
+struct hash {
+    int key;
+    int value;
+    UT_hash_handle hh;
 };
-struct s2 {
-    char a;
-    short b;
-    int c; //123
-};
+typedef struct 
+{
+    /* data */
+    int* arr;
+    int size;
+    struct hash* hashTable;
+}RandomizeSet;
+
+RandomizeSet* randomizeSetCreate() {
+    RandomizeSet* r = malloc(sizeof(RandomizeSet));
+    r->arr  = malloc(sizeof(int)*5000);
+    r->size = 0;
+    r->hashTable = NULL;
+    return r;
+}
+bool randomizedSetInsert(RandomizeSet* obj, int val) {
+    struct hash *h;
+    HASH_FIND_INT(obj->hashTable, &val, h);
+
+    if(!h) {
+        h = malloc(sizeof(struct hash));
+        h->key = val;
+        obj->arr[obj->size] = val;
+        h->value = obj->size;
+        obj->size++;
+        HASH_FIND_INT(obj->hashTable, h->key, h);
+        return true;
+    }
+    return false;
+}
+bool randomizeSetRemove(RandomizeSet* obj, int val) {
+    struct hash *h1, *h2;
+    HASH_FIND_INT(obj->hashTable, &val, h1);
+    
+    if(h1) {
+        obj->arr[h1->value] = obj->arr[obj->size-1];
+        HASH_FIND_INT(obj->hashTable, &obj->arr[obj->size-1],h2);
+        h2->value = h1->value;
+        obj->size--;
+        HASH_DEL(obj->hashTable,h1);
+        return true;
+    }
+    return false;
+}
+int randomizeSetGetRandom(RandomizeSet* obj) {
+    return obj->arr[rand()%obj->size];
+}
+void randomizeSetFree(RandomizeSet* obj) {
+    free(obj->arr);
+    free(obj);
+}
 int main(){
     //測試
     //test分支測試
