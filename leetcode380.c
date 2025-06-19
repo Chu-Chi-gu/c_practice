8507f45beef1d7e0bd154921181282de59c69b2c
Lacie <asdf4865123@gmail.com>
2025-06-19 23:30:38 +0800

    leetcode380手寫哈希表

--- leetcode380.c
+++ leetcode380.c
@@ -1,68 +1,96 @@
 #include <stdlib.h>
 #include <stdbool.h>
-#include "uthash.h"
-struct hash {
-    int key;
+
+#define MAX_SIZE 100000
+#define HASH_SIZE 10007
+
+typedef struct{
+    int idx;
     int value;
-    UT_hash_handle hh;
-};
+    bool used;
+}HashNode;
+
 typedef struct 
 {
-    /* data */
     int* arr;
     int size;
-    struct hash* hashTable;
+    HashNode* hash;
 }RandomizeSet;
+
+unsigned int hashFunc(int val) {
+    return abs(val)%HASH_SIZE;
+}
+int hashFind(HashNode* hash, int val) {
+    int h = hashFunc(val);
+    while(hash[h].used) {
+        if(hash[h].value == val) {
+            return h;
+        }
+        h = (h+1) % MAX_SIZE;
+    }
+    return h;
+}
 
 RandomizeSet* randomizeSetCreate() {
     RandomizeSet* r = malloc(sizeof(RandomizeSet));
-    r->arr  = malloc(sizeof(int)*5000);
+    r->arr  = malloc(sizeof(int)*MAX_SIZE);
     r->size = 0;
-    r->hashTable = NULL;
+    r->hash = calloc(HASH_SIZE,sizeof(HashNode));
     return r;
 }
 bool randomizedSetInsert(RandomizeSet* obj, int val) {
-    struct hash *h;
-    HASH_FIND_INT(obj->hashTable, &val, h);
-
-    if(!h) {
-        h = malloc(sizeof(struct hash));
-        h->key = val;
-        obj->arr[obj->size] = val;
-        h->value = obj->size;
-        obj->size++;
-        HASH_ADD_INT(obj->hashTable, key, h);
-        return true;
+    
+    int index = hashFind(obj->hash, val);
+    if(obj->hash[index].used && obj->hash[index].value == val) {
+        return false;
     }
-    return false;
+    obj->arr[obj->size]    = val;
+    obj->hash[index].value = val;
+    obj->hash[index].idx   = obj->size;
+    obj->hash[index].used  = true;
+    obj->size++;
+    return true;
 }
 bool randomizeSetRemove(RandomizeSet* obj, int val) {
-    struct hash *h1, *h2;
-    HASH_FIND_INT(obj->hashTable, &val, h1);
-    
-    if(h1) {
-        obj->arr[h1->value] = obj->arr[obj->size-1];
-        HASH_FIND_INT(obj->hashTable, &obj->arr[obj->size-1],h2);
-        h2->value = h1->value;
-        obj->size--;
-        HASH_DEL(obj->hashTable,h1);
-        return true;
+    int index = hashFind(obj->hash, val);
+    if( !(obj->hash[index].used) || obj->hash[index].value != val ) {
+        return false;
     }
-    return false;
+    
+    int idx     = obj->hash[index].idx;
+    int lastVal = obj->arr[obj->size - 1];
+
+    obj->arr[idx] = lastVal;
+
+    int lastH = hashFind(obj->hash, lastVal);
+    while( obj->hash[lastH].value != lastVal ) {
+        lastH = (lastH + 1) % MAX_SIZE;
+    }
+    obj->hash[lastH].idx = idx;
+
+    obj->hash[index].used = false;
+    obj->size--;
+    return true;
 }
 int randomizeSetGetRandom(RandomizeSet* obj) {
-    return obj->arr[rand()%obj->size];
+    int r = rand() % obj->size;
+    return obj->arr[r];
 }
 void randomizeSetFree(RandomizeSet* obj) {
     free(obj->arr);
+    free(obj->hash);
     free(obj);
 }
 int main(){
     int val_1 = 1;
     RandomizeSet* obj = randomizeSetCreate();
-    bool param_1 = randomizedSetInsert(obj,val_1);
-    bool param_2 = randomizeSetRemove(obj,val_1);
+    
+    bool param_1 = randomizedSetInsert(obj,3);
+    printf("param1=%d\n",param_1);
+    bool param_2 = randomizeSetRemove(obj,2);
+    printf("param2=%d\n",param_2);
     int  param_3 = randomizeSetGetRandom(obj);
+    printf("param3=%d\n",param_3);
     randomizeSetFree(obj);
    
     return 0;
